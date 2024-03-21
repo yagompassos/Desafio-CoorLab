5a2b533723740cde145924f812a9843484ed46ae
Yago <yagompassos@gmail.com>
2024-03-21 12:05:09 -0300

    CORS config for api consuming in frontend

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 import json
 
 from .models import City
 
-
 app = FastAPI()
 
+# Configurar o CORS para permitir solicitações de qualquer origem
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir solicitações de qualquer origem
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Métodos permitidos
+    allow_headers=["*"],  # Cabeçalhos permitidos
+)
+
+# Abrindo a base de dados em jason para leiutra
 with open('backend/data.json', 'r') as f:
     data = json.load(f)['transport']
 
@@ -14,18 +24,14 @@ with open('backend/data.json', 'r') as f:
 def get_root():
     return {"msg": "Funcionando"}
 
-@app.get("/cities", response_model=List[City])  # Especifique o modelo de resposta como List[City]
+# Endpoint para retornar a lista de cidades que estão com voos disponíveis
+@app.get("/cities", response_model=List[City])  
 async def get_cities():
-    # Conjunto para armazenar os nomes das cidades
     unique_cities = set()
 
-    # Itera sobre a lista de transportadoras no arquivo JSON
     for transport in data:
-        # Adiciona o nome da cidade ao conjunto
         unique_cities.add(transport['city'])
 
-    # Converte o conjunto de cidades de volta para uma lista
     cities = [{"cityName": city} for city in unique_cities]
 
-    # Retorna a lista de cidades
     return cities
